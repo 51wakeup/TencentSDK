b5a3478b0d15060c6b2dffd100e7c92e3c28bbc4
醒来－技术 <1020166296@qq.com>
2017-05-09 11:52:30 +0800

    useless

---
+++ Tencent/SDK/Useless.h
@@ -0,0 +1,13 @@
+//
+//  Useless.h
+//  Tencent
+//
+//  Created by 醒来－技术 on 2017/5/9.
+//  Copyright © 2017年 wakeup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Useless : NSObject
+
+@end
---
+++ Tencent/SDK/Useless.m
@@ -0,0 +1,13 @@
+//
+//  Useless.m
+//  Tencent
+//
+//  Created by 醒来－技术 on 2017/5/9.
+//  Copyright © 2017年 wakeup. All rights reserved.
+//
+
+#import "Useless.h"
+
+@implementation Useless
+
+@end
