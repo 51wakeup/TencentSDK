fafe133f419e2ba6122f42e2eaa0e4cbb5c5349e
醒来－技术 <1020166296@qq.com>
2017-05-09 11:00:56 +0800

    TencentHeader

---
+++ Tencent/SDK/TencentHeader.h
@@ -0,0 +1,20 @@
+//
+//  TencentHeader.h
+//  Tencent
+//
+//  Created by 醒来－技术 on 2017/5/9.
+//  Copyright © 2017年 wakeup. All rights reserved.
+//
+
+#ifndef TencentHeader_h
+#define TencentHeader_h
+
+
+#import <TencentOpenAPI/TencentMessageObject.h>
+#import <TencentOpenAPI/TencentOAuth.h>
+#import <TencentOpenAPI/TencentOAuthObject.h>
+#import <TencentOpenAPI/TencentApiInterface.h>
+#import "sdkDef.h"
+#import "QQApiInterface.h"
+#import "QQApiInterfaceObject.h"
+#endif /* TencentHeader_h */
