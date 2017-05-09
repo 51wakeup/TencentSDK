35a730765afa431da8077d6c04f4a3ddc33323f5
醒来－技术 <1020166296@qq.com>
2017-05-09 10:18:12 +0800

    manager

---
+++ Tencent/SDK/TencentManager.h
@@ -0,0 +1,13 @@
+//
+//  TencentManager.h
+//  Tencent
+//
+//  Created by 醒来－技术 on 2017/5/9.
+//  Copyright © 2017年 wakeup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TencentManager : NSObject
+
+@end
